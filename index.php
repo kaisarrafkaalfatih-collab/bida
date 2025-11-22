3583478e1b32cdf616fd9c69876f7821d14bacec
kaisarrafkaalfatih-collab <kaisarrafkaalfatih@gmail.com>
2025-11-23 06:45:29 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>BIDA - BIMAS Islam Kementerian Agama Kantor Kota Bandung</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="author" content="Bimas Islam Kota Bandung">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<meta name="description" content="BIDA- Bimbingan Masyarakat Islam (BIMAS) Kementerian Agama Kota Bandung Dalam Angka" />
+<meta name="keywords" content="Kementerian Agama,BIMAS, Bida, angka, Bimbingan Masyarakat Islam, Kota Bandung, Nikah, KUA, Zakat, Wakaf, Hisab, Arah Kiblat, Pangan Halal" />
+
+<link rel="shortcut icon" href="https://drive.google.com/uc?export=download&id=0BxQyd1ZAVR3NUW40eWlMeUVHYTg">
+<!--CSS styles-->
+
+<link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' rel='stylesheet'>
+<link href='bootstrap.css' rel='stylesheet'>
+<link rel="stylesheet" href="font-awesome.min.css">
+<link rel="stylesheet" href="perfect-scrollbar-0.4.5.min.css">
+<link rel="stylesheet" href="magnific-popup.css">
+<link rel="stylesheet" href="style.css">
+<link id="theme-style" rel="stylesheet" href="default.css">
+<link id="theme-style" rel="stylesheet" href="jquery.browser.css">
+
+<!--/CSS styles-->
+<!--Javascript files-->
+<script type="text/javascript" src="jquery-1.10.2.js"></script>
+<script type="text/javascript" src="TweenMax.min.js"></script>
+<script type="text/javascript" src="jquery.touchSwipe.min.js"></script>
+<script type="text/javascript" src="jquery.carouFredSel-6.2.1-packed.js"></script>
+
+<script type="text/javascript" src="modernizr.custom.63321.js"></script>
+<script type="text/javascript" src="jquery.dropdownit.js"></script>
+
+<script type="text/javascript" src="jquery.stellar.min.js"></script>
+<script type="text/javascript" src="ScrollToPlugin.min.js"></script>
+
+<script type="text/javascript" src="bootstrap.min.js"></script>
+
+<script type="text/javascript" src="jquery.mixitup.min.js"></script>
+
+<script type="text/javascript" src="masonry.min.js"></script>
+
+<script type="text/javascript" src="perfect-scrollbar-0.4.5.with-mousewheel.min.js"></script>
+
+<script type="text/javascript" src="magnific-popup.js"></script>
+<script type="text/javascript" src="custom.js"></script>
+
+<!--/Javascript files-->
+<script type="text/javascript">
+var BASE_URL ='data-bida/';
+var TAHUN ='2015';
+</script>
+<style type="text/css">
+#menu-daftarr-isi .col-md-6 ul li{
+cursor: pointer;
+}
+</style>
+
+</head>
+<body>
+<div id="wrapper">
+<a href="#sidebar" class="mobilemenu"><i class="icon-reorder"></i></a>
+
+<div id="sidebar" class="color-gradient">
+<div id="main-nav">
+<div id="nav-container" class="color-gradient">
+<div id="profile" class="clearfix">
+<!--<div class="portrate hidden-xs"></div>-->
+<img src="http://3.bp.blogspot.com/-yrPFl80d2DM/WBvgWpFbHII/AAAAAAAAAgc/bFFOlf85XNwcKYqSg_ETq9VC0wbOo9VhQCK4B/s1600/logo.png" />
+<div class="title">
+<h2>BIDA</h2>
+<h3 style="color:#fff;">KEMENTERIAN AGAMA KOTA BANDUNG</h3>
+</div>
+
+</div>
+<ul id="navigation">
+<li>
+<a href="#home">
+<div class="icon icon-home"></div>
+<div class="text">Home</div>
+</a>
+</li>
+
+<li>
+<a href="#profile_sambutan">
+<div class="icon icon-user"></div>
+<div class="text">Profile</div>
+</a>
+</li>
+
+<li>
+<a href="#daftar-isi" id="daftar_isi_link">
+<div class="icon icon-picture"></div>
+<div class="text">Data Bida</div>
+</a>
+</li>
+
+<li style="display:none;">
+<a href="#data-bida" id="daftar_bida_link" style="height:0px;">
+<div class="icon icon-time" style="height:0px;"></div>
+<div class="text" style="height:0px;">Data Bida</div>
+</a>
+</li>
+
+<li>
+<a href="#contact">
+<div class="icon icon-calendar"></div>
+<div class="text">Kontak Kami</div>
+
+</a>
+</li>
+
+
+
+</ul>
+
+</div>
+
+</div>
+
+<!--<div class="social-icons">
+<ul>
+
+</ul>
+</div>-->
+</div>
+
+<div id="main">
+<div id="home" class="page home" data-pos="home">
+<img src="http://4.bp.blogspot.com/-dmgCoLsMNQs/WBvf7N-ucdI/AAAAAAAAAgU/qhYf0h2ZF043Cynm1F1yzvpMG1SkGo_NQCK4B/s1600/Bida_FRONT2.jpg" width="100%" />
+<!--<div class="pageheader" >
+<div class="headercontent">
+<div class="section-container">
+<img src="cover.jpg" width="100%" />
+</div>
+</div>
+</div>-->
+</div>
+
+<div id="profile_sambutan" class="page">
+
+<div class="pageheader">
+
+<div class="headercontent">
+
+<div class="section-container">
+
+<h2 class="title" ><a href="#" class="backButton"></a>SEKSI BIMAS ISLAM</h2>
+
+<div class="row">
+<div class="col-md-9">
+<p>
+Puji syukur kami panjatkan ke hadlirat Alloh SWT., atas segala nikmat dan karunia-Nya, sehingga portal data Bimas Islam dalam Angka dapat kami selesaikan. Shalawat dan salam semoga selalu terlimpah kepada Penghulu Alam, Nabi Muhammad SAW., keluarganya, sahabatnya, dan semua umatnya hingga akhir zaman.
+</p><p>
+Saya, atas nama keluarga besar Seksi Bimas Islam, menyambut baik tersedinya portal data Bimas Islam dalam Angka ini. Kehadiran portal data ini merupakan bagian dari inovasi dan kesungguhan untuk memberikan pelayanan prima kepada masyarakat. Sehingga, masyarakat cukup membuka link website Kementerian Agama Kantor Kota Bandung, lalu berkunjung ke Portal Data Bimas Islam dalam Angka, tanpa harus bersusah payah datang ke kantor.
+</p><p>
+Kami berharap, semoga ke depan semakin banyak portal-portal serupa, yang membantu memudahkan masyarakat dalam mengkases data. Sehingga, kemudahan, kecepatan, keakuratan, dan ketersebaran informasi mengenai data tersebut bisa berlangsung lebih efektif.
+</p><p>
+Akhirnya, kami tetap mengaharapkan saran dan masukan yang konstruktif dari semua pihak. Tujuannya untuk melakukan penyempurnaan di masa yang akan datang. Selamat menikmati Bimas Islam dalam Angka.
+
+</p>
+</div>
+<div class="col-md-3">
+<center>
+<img src="http://4.bp.blogspot.com/-X28VlvK2Y5w/WBvg06xrSZI/AAAAAAAAAgs/sRzNThsMUVgpbMFnGRaGuD8CrMqpsOhqQCK4B/s1600/pakasi%2Bbimas.png" width="130" />
+<h5>Kasi BIMAS Islam</h5>
+<h6><b>H. Mimin Sutisna, M.Pd.I</b></h6>
+</center>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!--<div class="pagecontents">
+<div class="section color-1">
+<div class="section-container">
+<div class="title text-center">
+<h3>Stuktur Organisasi</h3>
+</div>
+<div class="row">
+
+</div>
+</div>
+</div>
+</div>-->
+
+</div>
+
+<div id="daftar-isi" class="page color-6 color-gradient" >
+<div class="pagecontents">
+
+<div class="section" style="border:none;">
+<div class="section-container" id="menu-daftarr-isi" style="font-size:12px">
+
+<h2 class="title" style="color:white;"><a href="#" class="backButton"></a> Data BIMAS Islam dalam Angka</h2>
+
+<div class="col-md-6">
+<div class="col-md-11">
+<div class="subtitle text-center">
+<h4 style="color:white;text-align:left;">Seksi BIMAS Islam Kota Bandung</h4>
+</div>
+<ul class="ul-boxed list-unstyled">
+<li rel="sekretariat_cart">Data KUA</li>
+<li rel="urais_cart">Data Penghulu</li>
+<li rel="penyuluh_cart">Data Penyuluh</li>
+<li rel="penais_cart">Peristiwa Nikah</li>
+<li rel="zakat_cart">Rumah Ibadah</li>
+<li rel="waqaf_cart">Jumlah Tanah Waqaf</li>
+<li rel="sakinah_cart">Keluarga Sakinah</li>
+
+
+</ul>
+</div>
+</div>
+<div class="col-md-6">
+<div class="col-md-11">
+<div class="subtitle text-center">
+<h4 style="color:white;text-align:left;">INTER AKTIF</h4>
+</div>
+<ul class="ul-boxed list-unstyled">
+<li rel="mtq_cart">M T Q</li>
+<li rel="simulasifaroidh_cart">Simulasi Faroidh</li>
+<li rel="simulasizakat_cart">Simulasi Zakat</li>
+
+</ul>
+</div>
+
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div id="data-bida" class="page color-6 color-gradient">
+<div class="pagecontents">
+
+<div class="section">
+<div class="section-container">
+
+<h2 class="title" id="title-data-bida" style="color:white;">Title</h2>
+<div class="col-md-12" id="content-data-bida">
+
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+<div id="contact" class="page stellar">
+<style type="text/css">
+table.form-contact tr td{
+padding: 5px;
+}
+</style>
+<div class="pageheader">
+<div class="headercontent">
+<div class="section-container">
+
+<h2 class="title"> <a href="#" class="backButton"></a> Kontak Kami</h2>
+
+
+<div class="col-md-9">
+<ul class="list-unstyled">
+<li>
+<strong><i class="icon-user"></i>&nbsp;&nbsp;</strong>
+<span>Bimas Islam Kota Bandung</span>
+</li>
+<li>
+<strong><i class="icon-phone"></i>&nbsp;&nbsp;</strong>
+
+<span>Telp: 022-7505925</span>
+</li>
+
+<li><span>Jl. Soekarno Hatta No. 498</span></li>
+<br/><br/><br/>
+<br/><br/><br/><br/><br/><br/><br/><br/>
+<li><span style="font-size:9px">Template By: simbi.kemenag.go.id</span>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div id="overlay"></div>
+
+</div>
+</div>
+<div id="mtq_cart" style="display:none;"><iframe src="mtq.html" border="0" framspacing="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" frameborder="0" height="600px" scrolling="no" width="100%"></iframe></div>
+<div id="zakat_cart" style="display:none;"><iframe src="ribadah.html" border="0" framspacing="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" frameborder="0" height="2780px" scrolling="no" width="100%"></iframe></div>
+<div id="urais_cart" style="display:none;"><iframe src="penghulu.html" border="0" framspacing="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" frameborder="0" height="2780px" scrolling="no" width="100%"></iframe></div>
+<div id="waqaf_cart" style="display:none;"><iframe src="wakaf.html" border="0" framspacing="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" frameborder="0" height="2780px" scrolling="no" width="100%"></iframe></div>
+<div id="penais_cart" style="display:none;"><iframe src="nikah.html" border="0" framspacing="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" frameborder="0" height="2780px" scrolling="no" width="100%"></iframe></div>
+<div id="sakinah_cart" style="display:none;"><iframe src="sakinah.html" border="0" framspacing="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" frameborder="0" height="2780px" scrolling="no" width="100%"></iframe></div>
+<div id="penyuluh_cart" style="display:none;"><iframe src="penyuluh.html" border="0" framspacing="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" frameborder="0" height="2780px" scrolling="no" width="100%"></iframe></div>
+<div id="simulasifaroidh_cart" style="display:none;"><iframe src="waris.html" border="0" framspacing="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" frameborder="0" height="2000px" scrolling="no" width="100%"></iframe></div>
+<div id="simulasizakat_cart" style="display:none;"><iframe src="simulasizakat.html" border="0" framspacing="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" frameborder="0" height="2700px" scrolling="no" width="100%"></iframe></div>
+<div id="sekretariat_cart" style="display:none;"><iframe src="DataKUA.html" border="0" framspacing="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" frameborder="0" height="2780px" scrolling="no" width="100%"></iframe>
+
+</div>
+<script language="javascript">
+URL_IMG_LOADING = 'loading.gif';
+URL_IMG_BACKBTN = 'http://1.bp.blogspot.com/-dlwLALI0dko/WBvgm8KI1GI/AAAAAAAAAgk/kTJT1s68bcUNl_pjpRO4gslnlY7zfaBbwCK4B/s1600/back.png';
+</script>
+<script type="text/javascript" src="bida.js"></script>
+</body>
+<!--Inspirasi Template dari http://simbi.kemenag.go.id/data-bida/-->
+</html>
